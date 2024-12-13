1740965e4e771ae04641c35e72f046173813427c
NotGayo <76450243+NotGayo@users.noreply.github.com>
2024-12-13 12:24:47 +0100

    Add files via upload

---
+++ VIDEOCLUB/src/model/Alquiler.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.util.*;
+
+//Clase Alquiler
+public class Alquiler {
+ private Date fechaHora;
+ private Pelicula pelicula;
+ private Usuario usuario;
+
+ // Getters y setters
+}
